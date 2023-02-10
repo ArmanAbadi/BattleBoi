3f5f5851c8b47c58f7d646b7357d95a1d0006885
Arman <arman_abadi@hotmail.com>
2023-02-09 23:52:02 -0800

    Setup room joining

---
+++ Assets/Networking/GameManager.cs
@@ -0,0 +1,47 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+	#region Public Fields
+
+	static public GameManager Instance;
+
+	#endregion
+
+	#region Private Fields
+
+	private GameObject instance;
+
+	[Tooltip("The prefab to use for representing the player")]
+	[SerializeField]
+	private GameObject playerPrefab;
+
+	#endregion
+
+	void Start()
+	{
+		Instance = this;
+
+		if (!PhotonNetwork.IsConnected)
+		{
+			return;
+		}
+
+		PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+	}
+	public override void OnLeftRoom()
+	{
+	}
+	public bool LeaveRoom()
+	{
+		return PhotonNetwork.LeaveRoom();
+	}
+
+	public void QuitApplication()
+	{
+		Application.Quit();
+	}
+}
