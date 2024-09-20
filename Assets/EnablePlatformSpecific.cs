bb5b859582d7bea6aa1634a6d77d9cafae1f2dac
Arman <arman_abadi@hotmail.com>
2024-09-19 22:27:02 -0700

    uodate

--- Assets/EnablePlatformSpecific.cs
+++ Assets/EnablePlatformSpecific.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnableIfPlatformSpecific : MonoBehaviour
+public class EnablePlatformSpecific : MonoBehaviour
 {
     public bool EnableIfMobile;
     public bool EnableIfDesktop;
